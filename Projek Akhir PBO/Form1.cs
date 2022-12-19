aa77ff31e9218860fc77f279da650f907b1f723b
Maulana-anjari <maulana17anjari@gmail.com>
2022-12-19 14:04:44 +0700

    feat: login

--- Projek Akhir PBO/Form1.cs
+++ Projek Akhir PBO/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Projek_Akhir_PBO
 {
@@ -20,6 +21,24 @@ namespace Projek_Akhir_PBO
         private void buttonClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-8PE9MPU2\SQLEXPRESS;Initial Catalog=dvfvwork;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("select count (*) from Table_User where username = '" + textBoxUsername.Text + "' and password = '" + textBoxPassword.Text + "'", conn );
+            DataTable dt = new DataTable();
+            sda.Fill( dt );
+            if (dt.Rows[0][0].ToString() == "1" )
+            {
+                this.Hide();
+                Home home = new Home();
+                home.Show();
+            }
+            else
+            {
+                MessageBox.Show("Username atau password salah!", "Perhatian!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
